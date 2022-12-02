8446b038c292cd5c3d192ea5bf8d6d56c36d3dcc
Alexis FERON <Alexis.FERON@etu.uca.fr>
2022-12-02 09:51:29 +0100

    FrontControlleur début

--- front-controleur/FrontControler.php
+++ front-controleur/FrontControler.php
@@ -1,9 +1,28 @@
 <?php
+require_once("controleur/ControleurUtilisateur.php");
+require_once("controleur/ControleurVisiteur.php");
+require_once("modeles/modeleConnexion.php");
+require_once("config/Validation.php");
+require("config/config.php");
 
-class FrontControler
-{
-    public function start()
-    {
+class FrontControler{
+    private $actions = array(
+        "Utilisateur" => [
+            "ajoutListePriv", "modifierListePriv", "afficherListePriv", "supprimerListePriv",
+            "deconnexion", "supprimerTachePriv", "modifierTachePriv", "modifierTachePriv"
+        ],
+        "Visiteur" => [
+            "seConnecter", "sInscrire", "tacheFaite",
+            "ajoutListePub", "modifierListePub", "afficherListePub", "supprimerListePub",
+            "supprimerTachePub", "modifierTachePub", "modifierTachePub"
+        ]
+    );
 
+    public function start(){
+        session_start();
+        $action = Validation::nettoyerString(isset($_GET["action"]) ? $_GET["action"] : "");
+        $controleur = new ControleurVisiteur();
+        $_REQUEST["action"] = "afficherListePub";
+        require("vues/accueil.php");
     }
 }
